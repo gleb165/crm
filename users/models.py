88669249bfab79ef541f2a34924491813cb6fe47
Глеб Савельев <glebsavelev@MacBook-Air-Gleb.local>
2026-01-21 11:56:39 +0100

    feat(app): Created an app for managing user permissions

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
